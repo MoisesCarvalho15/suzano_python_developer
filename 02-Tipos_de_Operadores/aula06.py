b64b3980e4fced1e23cb1b1a6ff0c7f9ffd4cb36
Moisés Santos Carvalho <mscarvalho1501@gmail.com>
2025-02-17 12:31:08 -0300

    Operadores Aritméticos

---
+++ 02-Tipos_de_Operadores/aula06.py
@@ -0,0 +1,9 @@
+# Operadores Aritméticos
+
+print(f"Soma: {10 + 20}")
+print(f"Subtração: {10 - 4}")
+print(f"Multiplicação: {10 * 2}")
+print(f"Divisão: {10 / 3}")
+print(f"Divisão Inteira: {10 // 3}")
+print(f"Módulo / Resto da divisão: {10 % 3}")
+print(f"Exponenciação: {5 ** 2}")
