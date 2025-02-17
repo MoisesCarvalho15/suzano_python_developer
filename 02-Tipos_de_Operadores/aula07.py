270af430f8dbe8710ad729960fbc8e7e8b2ef8fd
Moisés Santos Carvalho <mscarvalho1501@gmail.com>
2025-02-17 20:03:35 -0300

    Operadores de Comparação

---
+++ 02-Tipos_de_Operadores/aula07.py
@@ -0,0 +1,11 @@
+# Operadores de Comparação
+
+saldo = 200
+saque = 200
+
+print(f"Saque é igual ao saldo = {saque == saldo}")
+print(f"Saque é diferente do saldo = {saque != saldo}")
+print(f"Saque é maior do que o saldo = {saque > saldo}")
+print(f"Saque é maior ou igual ao saque = {saldo >= saque}")
+print(f"Saque é menor do que o saldo = {saque < saldo}")
+print(f"Saque é menor ou igual ao saldo = {saque <= saldo}")
