29abfcb61ac7031e4eb62a9c590511a9790b47ee
Moisés Santos Carvalho <mscarvalho1501@gmail.com>
2025-02-18 22:13:22 -0300

    Operadores de Associação

---
+++ 02-Tipos_de_Operadores/aula09.py
@@ -0,0 +1,10 @@
+# Operadores de Associação
+
+cursos = ["Python", "JavaScript", "Java", "PHP"]
+
+print(f"C# está na lista? {"R" in cursos}")
+print(f"Python está na lista? {"Python" in cursos}")
+print(f"JavaScript não está na lista? {"JavaScript" not in cursos}")
+
+# Case-sensitive
+print(f"python está na lista? {"python" in cursos}")
