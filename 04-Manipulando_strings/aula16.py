6d5ba427b9a59078c00967148770f7a6cf6f5141
Moisés Santos Carvalho <mscarvalho1501@gmail.com>
2025-02-24 08:08:58 -0300

    Fatiamento de Strings

---
+++ 04-Manipulando_strings/aula16.py
@@ -0,0 +1,20 @@
+# Fatiamento de Strings
+# start, stop, step
+
+#índice 012345...
+nome = "Moisés Santos Carvalho"
+
+# Pegando um caractere exato na string
+print(f"Terceiro caractere: {nome[2]}")
+
+# Pegando até um índice exato
+print(nome[:6])
+
+# Pegando o restante dos caracteres a partir de um outro
+print(nome[7:])
+
+# Invertendo os dados da string
+print(nome[::-1])
+
+# Pegando a string inteira e pulando de 2 em 2 caracteres
+print(nome[::2])
