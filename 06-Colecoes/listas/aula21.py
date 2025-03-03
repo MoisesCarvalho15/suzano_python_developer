eae5e38b2ad5ad26fffd6964e2c9a2d983215be1
Moisés Santos Carvalho <mscarvalho1501@gmail.com>
2025-03-03 05:48:47 -0300

    feat: Criando listas

---
+++ 06-Colecoes/listas/aula21.py
@@ -0,0 +1,12 @@
+# Criando listas
+
+frutas = ["Laranja", "Maçã", "Uva"]
+print(frutas)
+
+# Lista a partir de uma string
+letras = list("Python")
+print(letras)
+
+# Gerando uma lista de números através do range
+numeros = list(range(1, 11))
+print(numeros)
