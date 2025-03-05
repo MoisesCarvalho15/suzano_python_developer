cc3ef70ce125d279469e8f44ec4c5e9c2987cc8c
Moisés Santos Carvalho <mscarvalho1501@gmail.com>
2025-03-05 10:04:42 -0300

    feat: Métodos em tuplas

---
+++ 06-Colecoes/tuplas/aula27.py
@@ -0,0 +1,13 @@
+# Métodos em tuplas
+
+cores = ("vermelho", "azul", "verde", "azul",)
+
+# Contando o número que tal item aparece na tupla
+print(cores.count("vermelho"))
+print(cores.count("azul"))
+
+# Retornando a posição do item
+print(cores.index("verde"))
+
+# Retornando o tamanho total da tupla
+print(len(cores))
